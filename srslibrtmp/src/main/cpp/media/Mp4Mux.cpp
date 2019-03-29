ddb227bcf717ae53fda0b9a2fcb727256b300563
daikaimin <daikaimin@bilibili.com>
2019-03-29 19:44:44 +0800

    add mp4v2

---
+++ srslibrtmp/src/main/cpp/media/Mp4Mux.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kmdai on 2019/3/29.
+//
+
+#include "Mp4Mux.h"
---
+++ srslibrtmp/src/main/cpp/media/Mp4Mux.h
@@ -0,0 +1,15 @@
+//
+// Created by kmdai on 2019/3/29.
+//
+
+#ifndef LIBRTMP_MP4MUX_H
+#define LIBRTMP_MP4MUX_H
+
+#include "mp4v2/mp4v2.h"
+
+class Mp4Mux {
+
+};
+
+
+#endif //LIBRTMP_MP4MUX_H
